66a48c1e4c5d2a8798a01552507c0fa4bb6b1a2f
Noorzhan <jp_dev@mail.ru>
2017-03-22 13:57:15 +0600

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -1,93 +1,82 @@
-import time
-import json
 import socket
+import json
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileModifiedEvent
 
-with open('config.json') as json_file:
-    settings = json.load(json_file)
-
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_sock.connect(('127.0.0.1', 8000))
-connection_msg = '< ' + settings['name'] + ' connected > '
-client_sock.sendall(str.encode(connection_msg))
-
+# vars
 chat = []
-options = []
-my_messages = [connection_msg]
-template_reset = ''
+my_msg = []
+# vars
 
-with open ('template.txt') as file:
+with open('config.json') as json_file:
+    config = json.load(json_file)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.connect((config['ip'], config['port']))
+connection_msg = '< ' + config['name'] + ' connected >'
+client_socket.sendall(str.encode(connection_msg))
+my_msg.append(connection_msg)
+
+
+with open('template.txt') as file:
     template_reset = file.read()
 
 with open('chat.txt', 'w') as file:
-    file.write('< You logged in as ' + settings['name'] + ' >\n')
+    file.write('<! You logged in as: ' + config['name'] + ' !>\n')
     file.write(template_reset)
 
-class MyHandler(FileSystemEventHandler):
-    event = FileModifiedEvent('1.txt')
+# Слежка за файлом
+class ChatHandler(FileSystemEventHandler):
+    event = FileModifiedEvent('chat.txt')
+
     def on_modified(self, event):
         with open('chat.txt') as file:
             lines = file.readlines()
-            # name_str = lines[1][12:].rstrip()
-            name_str = settings['name']
+
+            name_str = config['name']
             try:
                 text_str = lines[5].strip()
                 options = lines[1].strip()
             except IndexError:
                 text_str = ''
                 options = ''
-
-            if 'disconnect' in options and my_messages[-1] != '< ' + settings['name'] + ' disconected >' :
-                disconnection_msg = '< ' + settings['name'] + ' disconected >'
-                client_sock.sendall(str.encode(disconnection_msg))
-                my_messages.append(disconnection_msg)
-
-            if 'clear' in options:
-                for msg in chat:
-                    chat.pop()
-                with open('chat.txt', 'w') as file:
-                    file.write('< You logged in as ' + settings['name'] + ' >\n')
-                    file.write(template_reset)
-
-            elif text_str != my_messages[-1] and name_str != '' and text_str != '' and my_messages[-1] != '< ' + settings['name'] + ' disconected >' :
-
-                client_sock.sendall(str.encode(name_str + ' > ' + text_str))
-                my_messages.append(text_str)
+            
+            # and text_str != my_msg[-1]
+            if text_str != '' and name_str != '':
+                client_socket.sendall(str.encode(name_str + '\t\t\t\t > ' + text_str))
 
 
-if __name__ == "__main__":
-    event_handler = MyHandler()
+# Получение информации
+if __name__ == '__main__':
+    event_handler = ChatHandler()
     observer = Observer()
-    observer.schedule(event_handler, path='.', recursive=False)
+    observer.schedule(event_handler, '.', recursive=False)
     observer.start()
 
     try:
         while True:
-            time.sleep(1)
-
-            if my_messages[-1] == '< ' + settings['name'] + ' disconected >':
-                raise KeyboardInterrupt
-
-            data = client_sock.recv(1024)
-
+            data = client_socket.recv(1024)
             if not data:
                 raise KeyboardInterrupt
+            # обработка информации
 
-            print('Ok: ', data.decode("utf-8"))
-            chat.append(data.decode("utf-8"))
-            print("\a")
+            chat.append(data.decode('utf-8'))
 
             with open('chat.txt', 'w') as file:
-                file.write('< You logged in as ' + settings['name'] + ' >\n')
+                file.write('<! You logged in as: ' + config['name'] + ' !>\n')
                 file.write(template_reset)
                 for msg in chat:
-                    file.write(msg.replace(settings['name'], 'You', 1) + '\n')
+                    file.write(msg + '\n')
+
 
 
     except KeyboardInterrupt:
-        client_sock.close()
+        disconnection_msg = '< ' + config['name'] + ' disconnected >'
+        client_socket.sendall(str.encode(disconnection_msg))
+        my_msg.append(disconnection_msg)
+        client_socket.close()
         with open('chat.txt', 'w') as file:
             file.write('')
         observer.stop()
+
     observer.join()
--- server/server.py
+++ server/server.py
@@ -1,22 +1,20 @@
 import socket
 
-chat = []
-
-serv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serv_sock.bind(('127.0.0.1', 8000))
-# serv_sock.setblocking(0)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('192.168.0.36', 5555 ))
+print('Server > 192.168.0.36:5555')
 clients = []
 
 while True:
-    data, addr = serv_sock.recvfrom(1024)
+    data, addr = server_socket.recvfrom(1024)
+
     if addr not in clients:
         clients.append(addr)
-    print(data.decode("utf-8"))
-    chat.append(data.decode("utf-8"))
+
+    # Обработка информации
+    print(data.decode('utf-8'))
+
     for client in clients:
-        serv_sock.sendto(data, client)
+        server_socket.sendto(data, client)
 
-    with open('log.txt', 'a') as logs:
-        pass
-
-serv_sock.close()
+server_socket.close()
